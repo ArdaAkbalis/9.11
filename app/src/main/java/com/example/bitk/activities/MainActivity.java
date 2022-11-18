4d108cb9279c1a057a9b85b263f7285d8291f8e9
ArdaAkbalis <ardaakbalis1@hotmail.com>
2022-11-18 16:24:57 +0300

    Mesleki Uygulama ve Proje Dersi(Bi Tık! projem)

--- app/src/main/java/com/example/bitk/activities/MainActivity.java
+++ app/src/main/java/com/example/bitk/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.bitk;
+package com.example.bitk.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.bitk.R;
 
 public class MainActivity extends AppCompatActivity {
 
